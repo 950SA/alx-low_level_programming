21cdacb0b60252922481c9fe21c7dc141ba2cef2
Sara Razazi <razazi1sara@gmail.com>
2023-02-21 15:39:40 -0800

    Macomit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,19 @@
 #include "main.h"
+/**
+ * print_alphabet - Entry point
+ * Description: print all alphabet in lowercase
+ * print_alphabet - prints the english alphabet from a-z.
+ * Return: Always 0 (success)
+ */
 
+void print_alphabet(void)
+{
+	char letter;
 
-	/**
-	 * print_alphabet - print all alphabet in lowercase
-	 */
-
-
-	void print_alphabet(void)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		char letter;
-
-
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-
-
-		_putchar('\n');
+		/* Description : print_alphabet call putchar from main*/
+		_putchar(letter);
 	}
+	_putchar('\n');
+}
